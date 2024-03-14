f7222d6df02559b22111ad0f1598a41dee27eb52
mi <zhangyiming748@gmail.com>
2024-03-14 17:13:54 +0800

    mkv添加srt内挂字幕是可行的

--- merge/merge.go
+++ merge/merge.go
@@ -48,22 +48,17 @@ func isExist(fp string) bool {
 }
 func getFilesWithExtension(folderPath string, extension string) ([]string, error) {
 	var files []string
-
 	err := filepath.Walk(folderPath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-
 		if !info.IsDir() && strings.HasSuffix(info.Name(), extension) {
 			files = append(files, path)
 		}
-
 		return nil
 	})
-
 	if err != nil {
 		return nil, err
 	}
-
 	return files, nil
 }
